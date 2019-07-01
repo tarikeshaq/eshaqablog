06f6f698efef7c4eeee1190f6f9bb4369383e284
Tarik <tarikishaq@hotmail.com>
2019-07-01 09:40:00 -0700

    finialzie technologies

--- components/about/Learner.jsx
+++ components/about/Learner.jsx
@@ -1,11 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const FigherContainer = styled.div`
+    position: absolute;
+    left: 75%;
+    top: 60%;
+    width: 20%;
+    height: 20%;
+    color: white;
+`;
+
 
 export default class Learner extends Component {
     render() {
         return (
-            <div>
-
-            </div>
+            <FigherContainer>
+                <h3>Learner</h3>
+                <div>
+                    <h5>University of British Columbia</h5>
+                    <ul>
+                        <li>Combined Major in Business and Computer Science</li>
+                        <li>Deans Honor List</li>
+                    </ul>
+                </div>
+            </FigherContainer>
         )
     }
 }
--- components/about/coder/ProgrammingLanguages.jsx
+++ components/about/coder/ProgrammingLanguages.jsx
@@ -12,24 +12,26 @@ export default class ProgrammingLanguages extends AnimatedContainer {
         return (
             <DelayedFadingContainer count={this.state.count} order={0}>
                 <h5>Programming Languages</h5>
-                <DelayedFadingSpan count={this.state.count} order={1}>
-                    <IconImage src="/static/images/gopher.png" roundedCircle fluid></IconImage>
-                </DelayedFadingSpan>
-                <DelayedFadingSpan count={this.state.count} order={2}>
-                    <IconImage src="/static/images/ferris.png" roundedCircle fluid></IconImage>
-                </DelayedFadingSpan>
-                <DelayedFadingSpan count={this.state.count} order={3}>
-                    <IconImage src="/static/images/javascript.png" roundedCircle fluid></IconImage>
-                </DelayedFadingSpan>
-                <DelayedFadingSpan count={this.state.count} order={4}>
-                    <IconImage src="/static/images/java.png" roundedCircle fluid></IconImage>
-                </DelayedFadingSpan>
-                <DelayedFadingSpan count={this.state.count} order={5}>
-                    <IconImage src="/static/images/cpp.png" roundedCircle fluid></IconImage>
-                </DelayedFadingSpan>
-                <DelayedFadingSpan count={this.state.count} order={6}>
-                    <IconImage src="/static/images/c.png" roundedCircle fluid></IconImage>
-                </DelayedFadingSpan>
+                <div>
+                    <DelayedFadingSpan count={this.state.count} order={1}>
+                        <IconImage src="/static/images/gopher.png" fluid></IconImage>
+                    </DelayedFadingSpan>
+                    <DelayedFadingSpan count={this.state.count} order={2}>
+                        <IconImage src="/static/images/ferris.png" fluid></IconImage>
+                    </DelayedFadingSpan>
+                    <DelayedFadingSpan count={this.state.count} order={3}>
+                        <IconImage src="/static/images/javascript.png" fluid></IconImage>
+                    </DelayedFadingSpan>
+                    <DelayedFadingSpan count={this.state.count} order={4}>
+                        <IconImage src="/static/images/java.png" fluid></IconImage>
+                    </DelayedFadingSpan>
+                    <DelayedFadingSpan count={this.state.count} order={5}>
+                        <IconImage src="/static/images/cpp.png" fluid></IconImage>
+                    </DelayedFadingSpan>
+                    <DelayedFadingSpan count={this.state.count} order={6}>
+                        <IconImage src="/static/images/c.png" fluid></IconImage>
+                    </DelayedFadingSpan>
+                </div>
             </DelayedFadingContainer>
         )
     }
