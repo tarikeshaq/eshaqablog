ccca65c03b34bc33bea897445c0fc911298fbbd5
Tarik <tarikishaq@hotmail.com>
2019-08-21 11:10:45 -0700

    fixed post title so that is aligns

--- components/posts/PostTitle.jsx
+++ components/posts/PostTitle.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
+const TitleContainer = styled.h1`
+margin: auto;
+text-align: center;
+font-weight: bold;
+font-size: 2.5rem;
+color: black;
+`;
 export default function PostTitle(props) {
   const { title } = props;
   return (
     <div>
-      <h5>{title}</h5>
+      <TitleContainer>{title}</TitleContainer>
     </div>
   );
 }
